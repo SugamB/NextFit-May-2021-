55af29c3cf73ef4126c15e7216722d95fbe7859a
HarPatel001 <hardik.patel2@mavs.uta.edu>
2021-03-31 16:01:32 -0500

    gets url and access token now

--- app/src/main/java/com/palhackmagic/nextfit/data/Landing.java
+++ app/src/main/java/com/palhackmagic/nextfit/data/Landing.java
@@ -14,8 +14,13 @@ import com.palhackmagic.nextfit.R;
 public class Landing extends AppCompatActivity {
 
     Button button1;
-    String url = "https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22C569&redirect_uri=https%3A%2F%2Flocalhost&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800";
-//maybe the response type can be changed to Code instead of "token"
+    String url = "https://www.fitbit.com/oauth2/authorize?" +
+                "response_type=token" +
+                "&client_id=22C569" +
+                "&redirect_uri=nextfit://logincallback" +
+                "&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight" +
+                "&expires_in=604800";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
---
+++ app/src/main/java/com/palhackmagic/nextfit/data/LoginCallBack.java
@@ -0,0 +1,34 @@
+package com.palhackmagic.nextfit.data;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginCallBack extends AppCompatActivity {
+
+    String string;
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        string = intent.getDataString();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        onNewIntent(getIntent());
+
+        String accessToken = string.substring(string.indexOf("&access_token") + 14);
+        String userId = string.substring(string.indexOf("&user_id")+9, string.indexOf("&token_type"));
+        String tokenType = string.substring(string.indexOf("&token_type")+12,string.indexOf("&expires_in"));
+
+        Log.i("TAG", string);
+        Log.i("TAG", accessToken);
+        Log.i("TAG", userId);
+        Log.i("TAG", tokenType);
+    }
+}
