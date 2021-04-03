43734bb0c110c67ab599c1d0985bb98b54afc579
sugam <london_sugam@yahoo.com>
2021-04-02 21:51:43 -0500

    testapi.java retrieves user data and displays in logcat for now

--- app/src/main/java/com/palhackmagic/nextfit/data/LoginCallBack.java
+++ app/src/main/java/com/palhackmagic/nextfit/data/LoginCallBack.java
@@ -21,14 +21,25 @@ public class LoginCallBack extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         onNewIntent(getIntent());
-
-        String accessToken = string.substring(string.indexOf("&access_token") + 14);
-        String userId = string.substring(string.indexOf("&user_id")+9, string.indexOf("&token_type"));
+        String accessToken = string.substring(string.indexOf("#access_token") + 14, string.indexOf("&user_id"));
+        String userId = string.substring(string.indexOf("&user_id")+9, string.indexOf("&scope"));
         String tokenType = string.substring(string.indexOf("&token_type")+12,string.indexOf("&expires_in"));
+
+//        String accessToken = string.substring(string.indexOf("&access_token") + 14);
+//        String userId = string.substring(string.indexOf("&user_id")+9, string.indexOf("&token_type"));
+//        String tokenType = string.substring(string.indexOf("&token_type")+12,string.indexOf("&expires_in"));
 
         Log.i("TAG", string);
         Log.i("TAG", accessToken);
         Log.i("TAG", userId);
         Log.i("TAG", tokenType);
+
+        Intent intent = new Intent(getApplicationContext(), Testapi.class);
+        intent.putExtra("string", string);
+        intent.putExtra("accessToken",accessToken);
+        intent.putExtra("userId",userId);
+        intent.putExtra("tokenType",tokenType);
+        startActivity(intent);
+
     }
 }
